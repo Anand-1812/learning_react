f366cce7138fb9a8b6ecb1a95ed4a84ff5b348d8
Anand-1812 <anand.work2005@gmail.com>
2025-07-10 21:58:56 +0530

    learning and screwing around

--- src/Rendering.jsx
+++ src/Rendering.jsx
@@ -12,26 +12,10 @@ function Button(props) {
   );
 }
 
-function handleButtonClick() {
-  return (
-    <ul>
-      <li>Class</li>
-      <li>Study</li>
-      <li>sleep</li>
-    </ul>
-  );
-}
-
 export function Rendering() {
   return (
     <div>
-      <Button
-        text="Buy your thing"
-        color="#808080"
-        fontSize={12}
-        padding={8}
-        handleClick={handleButtonClick}
-      />
+      <Button text="Buy your thing" color="#808080" fontSize={12} padding={8} />
     </div>
   );
 }
