e1e1429d123d2f66f29f7cb73c66c963d6471713
Anand-1812 <anand.work2005@gmail.com>
2025-07-15 21:28:07 +0530

    learned how to share data between components

--- src/Rendering.jsx
+++ src/Rendering.jsx
@@ -1,7 +1,41 @@
+import { useState } from "react";
+
 export function Rendering() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const fullName = firstName + " " + lastName;
+
+  const [age, setAge] = useState(59);
+
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value);
+  }
+
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
+  }
+
+  const handleIncreaseAge = () => {
+    setAge(age + 1);
+  };
+
   return (
-    <div>
-      <h3>Hello</h3>
-    </div>
+    <>
+      <label>
+        First name: <input value={firstName} onChange={handleFirstNameChange} />
+      </label>
+      <label>
+        Last Name: <input value={lastName} onChange={handleLastNameChange} />
+      </label>
+
+      <div>
+        <h1>Person Details</h1>
+        <p>{fullName}</p>
+        <p>{age}</p>
+      </div>
+
+      <button onClick={handleIncreaseAge}>Increase Age</button>
+    </>
   );
 }
