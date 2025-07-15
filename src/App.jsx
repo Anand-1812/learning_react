6b384c80bfcfd3a17fa9d7a9ace056714a245821
Anand-1812 <anand.work2005@gmail.com>
2025-07-15 19:02:43 +0530

    sharing state between components

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,40 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {
-  const [isSent, setIsSent] = useState(false);
-  const [message, setMessage] = useState("Hi!");
-
-  if (isSent) {
-    return <h1>Your message is on it's way!</h1>;
-  }
-
+function Panel({ title, children, isActive, onShow }) {
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        setIsSent(true);
-        sendMessage(message);
-      }}
-    >
-      <textarea
-        placeholder="Message"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <button type="submit">Send</button>
-    </form>
+    <section className="panel">
+      <h3>{title}</h3>
+      {isActive ? <p>{children}</p> : <button onClick={onShow}>Show</button>}
+    </section>
   );
 }
 
-function sendMessage(message) {}
+export default function App() {
+  const [activeIndex, setActiveIndex] = useState(0);
 
-export default App;
+  return (
+    <>
+      <h2>Almaty, kazakhstan</h2>
+      <Panel
+        title="About"
+        isActive={activeIndex === 0}
+        onShow={() => setActiveIndex(0)}
+      >
+        with a population of about 2 millions, Almaty is kazakshtan's largest
+        city. from 1929 to 1997, it was its capital city.
+      </Panel>
+      <Panel
+        title="Etymology"
+        isActive={activeIndex === 1}
+        onShow={() => setActiveIndex(1)}
+      >
+        The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for
+        "apple" and is often translated as "full of apples". In fact, the region
+        surrounding Almaty is thought to be the ancestral home of the apple, and
+        the wild <i lang="la">Malus sieversii</i> is considered a likely
+        candidate for the ancestor of the modern domestic apple.
+      </Panel>
+    </>
+  );
+}
