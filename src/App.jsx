7443cde4f4369c12cf24829dbd54220a7de88e28
Anand-1812 <anand.work2005@gmail.com>
2025-07-15 11:03:52 +0530

    learning more about state

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,21 @@
 import { useState } from "react";
 import "./App.css";
 
-const COLORS = ["black", "grey", "orange", "yellow"];
-
 function App() {
-  const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
+  const [person, setPerson] = useState({ name: "johh", age: 54 });
 
-  const onButtonClick = (color) => {
-    setBackgroundColor(color);
+  const handleIncreaseAge = () => {
+    console.log("In handle increase age before set person call", person);
+    setPerson({ ...person, age: person.age + 1 });
+    console.log("After set person call", person);
   };
 
   return (
-    <div className="App" style={{ backgroundColor }}>
-      {COLORS.map((color) => (
-        <button
-          type="button"
-          key={color}
-          onClick={() => onButtonClick(color)}
-          className={backgroundColor === color ? "selected" : ""}
-        >
-          {color}
-        </button>
-      ))}
-    </div>
+    <>
+      <h1>{person.name}</h1>
+      <h2>{person.age}</h2>
+      <button onClick={handleIncreaseAge}>Increase age</button>
+    </>
   );
 }
 
--- src/Rendering.jsx
+++ src/Rendering.jsx
@@ -1,21 +1,7 @@
-function Button(props) {
-  const btnStyle = {
-    color: props.color,
-    fontSize: props.fontSize + "px",
-    padding: props.padding + "px",
-  };
-
-  return (
-    <button onClick={props.handleClick} style={btnStyle}>
-      {props.text}
-    </button>
-  );
-}
-
 export function Rendering() {
   return (
     <div>
-      <Button text="Buy your thing" color="#808080" fontSize={12} padding={8} />
+      <h3>Hello</h3>
     </div>
   );
 }
