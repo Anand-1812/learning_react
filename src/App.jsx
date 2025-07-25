1ba745e6e963598b4e9fc8c487cb6c021e4c86ec
Anand <anand.work2005@gmail.com>
2025-07-25 21:16:36 +0530

    class based components

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,6 @@ import {useState } from "react";
 import "./App.css";
 import ChatRoom from "./effects";
 
-function Panel({ title, children, isActive, onShow }) {
-  return (
-    <section className="panel">
-      <h3>{title}</h3>
-      {isActive ? <p>{children}</p> : <button onClick={onShow}>Show</button>}
-    </section>
-  );
-}
-
 export default function App() {
   const [activeIndex, setActiveIndex] = useState(0);
 
@@ -21,29 +12,6 @@ export default function App() {
 
   return (
     <>
-      <div>
-        <h2>Almaty, kazakhstan</h2>
-        <Panel
-          title="About"
-          isActive={activeIndex === 0}
-          onShow={() => setActiveIndex(0)}
-        >
-          with a population of about 2 millions, Almaty is kazakshtan's largest
-          city. from 1929 to 1997, it was its capital city.
-        </Panel>
-        <Panel
-          title="Etymology"
-          isActive={activeIndex === 1}
-          onShow={() => setActiveIndex(1)}
-        >
-          The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for
-          "apple" and is often translated as "full of apples". In fact, the region
-          surrounding Almaty is thought to be the ancestral home of the apple, and
-          the wild <i lang="la">Malus sieversii</i> is considered a likely
-          candidate for the ancestor of the modern domestic apple.
-        </Panel>
-      </div>
-
       <div>
         <label>
           Choose the chat room:{' '}
